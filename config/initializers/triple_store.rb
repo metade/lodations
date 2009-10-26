caa5f67996fedb60430332c007b91e2446674db7
Patrick Sinclair <metade@livro.local>
2009-10-26 11:47:35 -0400

    Trying to get activerdf to work on heroku

--- config/initializers/triple_store.rb
+++ config/initializers/triple_store.rb
@@ -2,3 +2,4 @@ require File.join(RAILS_ROOT, 'lib', 'store')
 
 # STORE = RDFLiteStore.new(nil)
 
+require File.join(RAILS_ROOT, 'vendor', 'plugins', 'activerdf_sparql', 'lib', 'activerdf_sparql', 'init.rb')
