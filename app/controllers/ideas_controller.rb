cc2a071431e157579c5b1602a243fedee94d3769
Patrick Sinclair <metade@livro.local>
2009-10-27 17:21:23 -0400

    Listing number of recommendations

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -3,7 +3,7 @@ class IdeasController < ApplicationController
   # GET /ideas.xml
   def index
     @ideas = Idea.all
-
+    
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @ideas }
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,3 +1,7 @@
 class Idea < ActiveRecord::Base
   has_many :canned_queries
+  
+  def recommendation_count
+    canned_queries.inject(0) {|sum, q| sum + q.recommendations.count }
+  end
 end
